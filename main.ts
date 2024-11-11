c7952019d41003ed2ba1c4ca714b88c2471fe703
Jonathan <titanbar@hotmail.com>
2024-11-11 23:14:00 +0100

    Mise à jour pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -9,16 +9,28 @@ function showKO(){
     basic.showIcon(IconNames.Sad)
 }
 
-showKO()
 
 function checkTest1(){
-
+    showOK()
 }
 
 function checkTest2() {
-
+    showOK()
 }
 
 function checkTest3() {
-
+    showOK()
 }
+
+let direction = 10;
+let brightness = 100;
+
+function pulseBright() {
+    if ((direction === 10 && brightness > 255) || (direction === -10 && brightness < 10)) {
+        direction *= -1;
+    }
+    brightness += direction;
+    led.setBrightness(brightness);
+}
+
+pulseBright();
